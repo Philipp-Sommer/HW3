9b8213a79fe40949382fb4ade0034486c73006a2
Philipp Sommer <philipp.sommer@edu.uni-graz.at>
2018-04-16 19:30:04 +0200

    removed print statements

--- vrptw_reader.py
+++ vrptw_reader.py
@@ -49,7 +49,7 @@ def calc_distance(string_list, CUST_NO1, CUST_NO2):
     y1 = float(rel_string1_spl[2])
     x2 = float(rel_string2_spl[1])
     y2 = float(rel_string2_spl[2])
-    print(x1, x2, y1, y2)
+    #print(x1, x2, y1, y2)
     distance = ((y1 - y2)**2 + (x1 - x2)**2)**(1/2)
     return distance
 
